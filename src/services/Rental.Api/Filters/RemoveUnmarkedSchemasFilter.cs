cabf74b44c430311789c59d2f5b966a4e80a4caa
Renan Gobbi <renaneg2019@hotmail.com>
2025-11-13 10:40:16 -0300

    fix(swagger): prevent removal of referenced schemas during filtering

--- src/services/Rental.Api/Filters/RemoveUnmarkedSchemasFilter.cs
+++ src/services/Rental.Api/Filters/RemoveUnmarkedSchemasFilter.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.OpenApi.Models;
 using Rental.Api.Swagger;
 using Swashbuckle.AspNetCore.SwaggerGen;
-using System.Collections.Concurrent;
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Rental.Api.Filters
@@ -16,21 +17,80 @@ namespace Rental.Api.Filters
             public void Apply(OpenApiSchema schema, SchemaFilterContext context)
             {
                 var schemaId = context.Type.Name;
-
                 SchemaTypes[schemaId] = context.Type;
             }
         }
 
         public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
         {
+            var referencedSchemas = new HashSet<string>(StringComparer.Ordinal);
+
+            void CollectSchemaRefs(OpenApiSchema schema)
+            {
+                if (schema == null)
+                    return;
+
+                if (schema.Reference?.Id != null)
+                    referencedSchemas.Add(schema.Reference.Id);
+
+                if (schema.Items != null)
+                    CollectSchemaRefs(schema.Items);
+
+                if (schema.Properties != null)
+                {
+                    foreach (var prop in schema.Properties.Values)
+                        CollectSchemaRefs(prop);
+                }
+
+                if (schema.AllOf != null)
+                {
+                    foreach (var s in schema.AllOf)
+                        CollectSchemaRefs(s);
+                }
+
+                if (schema.OneOf != null)
+                {
+                    foreach (var s in schema.OneOf)
+                        CollectSchemaRefs(s);
+                }
+
+                if (schema.AnyOf != null)
+                {
+                    foreach (var s in schema.AnyOf)
+                        CollectSchemaRefs(s);
+                }
+            }
+
+            foreach (var pathItem in swaggerDoc.Paths.Values)
+            {
+                foreach (var op in pathItem.Operations.Values)
+                {
+                    if (op.RequestBody != null)
+                    {
+                        foreach (var content in op.RequestBody.Content.Values)
+                            CollectSchemaRefs(content.Schema);
+                    }
+
+                    foreach (var response in op.Responses.Values)
+                    {
+                        foreach (var content in response.Content.Values)
+                            CollectSchemaRefs(content.Schema);
+                    }
+                }
+            }
+
             var toRemove = swaggerDoc.Components.Schemas
                 .Where(kvp =>
                 {
-                    if (SchemaTypeRegistryFilter.SchemaTypes.TryGetValue(kvp.Key, out var type))
-                    {
-                        return !typeof(IExposeInSwagger).IsAssignableFrom(type);
-                    }
-                    return true;
+                    var schemaName = kvp.Key;
+
+                    if (referencedSchemas.Contains(schemaName))
+                        return false;
+
+                    if (!SchemaTypeRegistryFilter.SchemaTypes.TryGetValue(schemaName, out var type))
+                        return false;
+
+                    return !typeof(IExposeInSwagger).IsAssignableFrom(type);
                 })
                 .Select(kvp => kvp.Key)
                 .ToList();
@@ -42,3 +102,4 @@ namespace Rental.Api.Filters
         }
     }
 }
+
